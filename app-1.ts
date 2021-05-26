939141590e97894e0942207901a01d4b1d847791
ANDRES GERARDO AYALA SANCHEZ <ayalaag@HILDEBOG.LOCAL>
2021-05-25 20:39:20 -0500

    prueba de decrementar

--- app-1.ts
+++ app-1.ts
@@ -1,27 +1,34 @@
 interface Action {
-    type: string;
-    payload?: any; //Parámetros que uno le puede enviar a la acción
+  type: string;
+  payload?: any; //Parámetros que uno le puede enviar a la acción
+}
+
+// Reducer --> Siempre debe regresar un estado
+// No interactua con librería externas
+function reducer(state = 10, action: Action) {
+
+  switch (action.type) {
+    case 'INCREMENTAR':
+      return (state += action.payload);
+
+    case 'DECREMENTAR':
+      return (state -= action.payload);
+    default:
+      return state;
   }
-  
-  // Reducer --> Siempre debe regresar un estado
-  // No interactua con librería externas
-  function reducer(state = 10, action: Action) {
-    
-    switch (action.type) {
-        case 'INCREMENTAR':
-            return (state += action.payload);
-    
-        default:
-            return state;
-    }
-  
-  }
-  
-  // Usar el reducer
-  const incrementadorAction: Action = {
-    type: 'INCREMENTAR',
-    payload: 5
-  };
-  
-  console.log(reducer(10, incrementadorAction));
-  
+
+
+}
+
+// Usar el reducer
+const incrementadorAction: Action = {
+  type: 'INCREMENTAR',
+  payload: 5
+};
+const decrementadorAction: Action = {
+  type: 'DECREMENTAR',
+  payload: 3
+};
+
+console.log(reducer(10, incrementadorAction));
+console.log(reducer(10, decrementadorAction));
