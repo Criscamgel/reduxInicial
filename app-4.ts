0ed3f200188dd3c546a8cee9d532f932c8cc1657
Criscamgel <caguio@ucompensar.edu.co>
2021-05-25 21:37:17 -0500

    Instalando el Store e independizandolo

---
+++ app-4.ts
@@ -0,0 +1,12 @@
+import { createStore, Store } from "redux";
+import { contadorReducer } from "./contador/contador.reducer";
+
+
+const store: Store = createStore( contadorReducer );
+
+store.subscribe( () => {
+    console.log( 'subs:', store.getState() );
+} )
+
+
+store.dispatch( incrementadorAction );
