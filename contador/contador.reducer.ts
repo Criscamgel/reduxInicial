1314b4735efd7127ecef0dfd3db3e02a5e4ba248
andress20 <andress20@gmail.com>
2021-05-25 21:23:46 -0500

    add reducers app3

--- contador/contador.reducer.ts
+++ contador/contador.reducer.ts
@@ -5,6 +5,15 @@ export function contadorReducer(state = 10, action: Action) {
     switch (action.type) {
         case 'INCREMENTAR':
             return (state += action.payload);
+            
+        case "DECREMENTAR":
+            return (state -= action.payload);
+        
+        case "MULTIPLICAR":
+        return (state *= action.payload);
+    
+        case "DIVIDIR":
+        return (state /= action.payload);
     
         default:
             return state;
